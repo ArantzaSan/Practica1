00674c31fd4206a506d439ab2591b7fe55a78904
ArantzaSan <143145265+ArantzaSan@users.noreply.github.com>
2025-03-14 11:09:45 -0600

    Create Floats, listas y posiciones negativas.py

---
+++ Floats, listas y posiciones negativas.py
@@ -0,0 +1,14 @@
+#Float
+num1 = 10.45
+num2 = 45.9768
+print(num1+num2)
+# para  redondear round()
+num1 = 10.45
+num2 = 45.9768
+resultado = num1+num2
+print(round(resultado,2))
+#Listas, la posición 0 sería Xiaomi
+smartphones = ['Xiaomi', 'Iphone', 'Huawei', 'Samsung']
+print(smartphones[2])
+#Posición negativa para encontrar de otra manera el dato de la lista
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
