6edf0f125e821f54544b88ae2693bbce9516deeb
ArantzaSan <143145265+ArantzaSan@users.noreply.github.com>
2025-03-14 10:46:26 -0600

    Create Create Concatenación, títulos y salto de línea.py

---
+++ Create Concatenación, títulos y salto de línea.py
@@ -0,0 +1,22 @@
+#Concatenación
+palabra_1 = 'Programación'
+palabra_2 = 'facil'
+nombre_completo = palabra_1 + palabra_2
+print(nombre_completo)
+#Concatenación de números
+num1= '10'
+num2 = '15'
+suma = num1 + num2
+print(suma)
+#Título
+nombre = "programación fácil"
+nombre = nombre.title()
+print(nombre)
+#Título upper()
+nombre = "programación fácil".upper()
+print(nombre)
+#Título lower()
+nombre = "PrOGRaMacion faCIL.lower()
+#Saltos de línea en Python
+cursos = "Lista dee cursos de programación:\n\tPython\n\tJavaScript\n\tJava\n\tPHP\n\tTypeScript"
+print(cursos)
