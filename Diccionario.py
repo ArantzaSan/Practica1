153f1cb4619bca13c3a5091bcbd116eaa9c402cf
ArantzaSan <143145265+ArantzaSan@users.noreply.github.com>
2025-03-16 10:38:53 -0600

    Create Diccionario.py

---
+++ Diccionario.py
@@ -0,0 +1,49 @@
+#crear un diccionario
+teclado1 = {
+'Categoría': 'teclados',
+'Modelo': 'Hyper alloy FPS Pro',
+'Precio': '89,99'
+}
+teclado2= {
+'Categoría': 'teclados',
+'Modelo': 'Corsair K55 RGB',
+'Precio': '59,99'
+}
+print(teclado1)
+#Modificar valores de un diccionario
+teclado1 = {
+'Categoría': 'teclados',
+'Modelo': 'Hyper alloy FPS Pro',
+'Precio': '89,99'
+}
+teclado2= {
+'Categoría': 'teclados',
+'Modelo': 'Corsair K55 RGB',
+'Precio': '59,99'
+}
+teclado1['Precio'] = '85'
+print(teclado1['Precio'])
+#Método len()
+teclado1 = {
+'Categoría': 'teclados',
+'Modelo': 'Hyper alloy FPS Pro',
+'Precio': '89,99'
+}
+print(len(teclado))
+#Método para eliminar con del
+teclado1 = {
+'Categoría': 'teclados',
+'Modelo': 'Hyper alloy FPS Pro',
+'Precio': '89,99'
+}
+del teclado1['Precio']
+print(teclado)
+#Método para añadir
+teclado1 = {
+'Categoría': 'teclados',
+'Modelo': 'Hyper alloy FPS Pro',
+'Precio': '89,99'
+}
+teclado1['Color'] = 'Negro
+print(teclado)
+
