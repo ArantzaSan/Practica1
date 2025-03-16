74aa83cc81450795fb063aa85e0ed8d1ed54bbd0
ArantzaSan <143145265+ArantzaSan@users.noreply.github.com>
2025-03-16 09:02:34 -0600

    Create Bucles.py

---
+++ Bucles.py
@@ -0,0 +1,25 @@
+#bucle while
+x = 1
+
+while x < 10:
+  print(x)
+#bucle while con break
+x = 1
+while x<= 10:
+  print(x)
+if x == 5:
+  break
+x += 1
+##bucle while con continue
+x = 1
+while x<= 10:
+x += 1
+if x == 5 or x == 7:
+  continue
+print(x)
+#Bucle for
+for x in 'Python':
+print(x)
+#función range() en bucles
+for x in range(5,10):
+print(x)
