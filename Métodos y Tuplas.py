569ed9eba3f478b8ce73667823e8e8d87b4b1267
ArantzaSan <143145265+ArantzaSan@users.noreply.github.com>
2025-03-16 08:40:57 -0600

    Create Métodos y Tuplas.py

---
+++ Métodos y Tuplas.py
@@ -0,0 +1,18 @@
+#El uso de los métodos sort() para ordenar listas
+colores = ['rojo', 'azul', 'verde', 'amarillo]
+colores.sort()
+print(colores)
+#El uso de los métodos sorted() para ordenar listas
+colores = ['rojo', 'azul', 'verde', 'amarillo]
+print(sorted(colores))
+print(colores)
+#Contar la longitud de elementos de una lista
+colores = ['rojo', 'azul', 'verde', 'amarillo]
+print(len(colores))
+#Diferencias entre listas y tuplas, tuplas son con paréntesis
+tupla = 810,15,20, 'El resultado de la operación es:')
+print(tupla[3], tupla[2] + tupls[0] * tupla[1] / tupla[0])
+#Cómo convertir una lista en una tupla
+lista = ['rojo', 'azul', 'verde', 'amarillo]
+tupla = tuple(lista)
+print(tupla)
