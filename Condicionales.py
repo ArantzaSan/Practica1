6d24e80497b89c67592fa20cb2226847eb83a4be
ArantzaSan <143145265+ArantzaSan@users.noreply.github.com>
2025-03-16 08:55:37 -0600

    Create Condicionales.py

---
+++ Condicionales.py
@@ -0,0 +1,35 @@
+# condicional IF 
+num1 = 10
+num2 = 20
+
+if num1 == num2:
+  print('Se ejecuta el if')
+# condicional IF con else
+edad = 20
+if edad >= 18:
+  print('Puedes accedder, eres mayor de edad')
+else:
+  print('No puedes acceder')
+#if elif else e input
+edad = int(input('cual es tu edad'\n')
+if edad <= 0:
+  print('Nadie puede tener esa edad')
+elif edad <= 1 and edad <= 18:
+else:
+  print('edad no válida')
+#buscar resultados en listas y tuplas
+navegadores = ['chrome, 'firefox', 'opera', 'safari']
+print('çhrome' in navegadores)
+#múltiples IF sin elif
+entrada = int(input('Introduce un número del 1 al 3'))
+if entrada == 1:
+  print(´Seleccionaste 1')
+if entrada == 2:
+  print('Seleccionaste 2')
+if entrada == 3:
+  print('Seleccionaste 3')
+#Formas de abreviar condicionales
+x = 100
+y = 200
+if x < y: print('x es menor que y')
+
