1d82ae57522d0290a058c35e7439ea290aea2704
ArantzaSan <143145265+ArantzaSan@users.noreply.github.com>
2025-03-17 12:03:46 -0600

    Update Funciones, argumentos y clases.py

--- Funciones, argumentos y clases.py
+++ Funciones, argumentos y clases.py
@@ -22,4 +22,68 @@ usuario001 = Usuario()
 usuario001.nombre = 'Enrique'
 usuario001.apellidos = 'Barros Fernández'
 usuario001.imprime_datos()
+#método _init_
+class Usuarios:
+    def_init_(self, nombre, apellidos)
+    self.nombre = nombre
+    self.apellidos = apellidos
 
+  def imprime_datos(self):
+    print('Nombre:', self.nombre, '\nApellidos:', self.apellidos)
+
+usuario001 = Usuario()
+#self para clases
+class Usuarios:
+    def_init_(self, nombre, apellidos)
+    self.nombre = nombre
+    self.apellidos = apellidos
+
+  def imprime_datos(self):
+    print('Nombre:', self.nombre, '\nApellidos:', self.apellidos)
+
+usuario001 = Usuario('Enrique', 'Barros Fernández')
+
+usuario002 = Usuario('Javier', 'Gomila Reyes')
+
+usuario002.nombre = 'Jacinto'
+
+usuario002.imprime_datos()
+#Declaración de clases vacías
+# pass
+class NombreClase
+  pass
+  #eliminar atributos de objetos
+del nombre_objeto.atributo
+  #eliminar objetos
+del nombre_objeto
+#Herencia de clases
+  #esta es la superclase
+class Usuarios:
+    def_init_(self, nombre, apellidos)
+    self.nombre = nombre
+    self.apellidos = apellidos
+
+  def imprime_datos(self):
+    print('Nombre:', self.nombre, '\nApellidos:', self.apellidos)
+    #esta es la subclase
+  class UsuariosPremium(Usuarios):
+    pass
+  #como heredar init
+#superclase
+class Usuarios:
+    def_init_(self, nombre, apellidos)
+    self.nombre = nombre
+    self.apellidos = apellidos
+
+  def imprime_datos(self):
+    print('Nombre:', self.nombre, '\nApellidos:', self.apellidos)
+    #subclase
+class UsuariosPremium(Usuarios):
+  def_init_(self, nombre, apelllidos, instagram)
+  self.nombre = nombre
+  self.apellidos = apellidos
+  self.instagram = instagram
+  
+
+    
+    
